07f74a7ac3acb326ff50db1e4ea379754270d54e
Your Name Here <agui150@usc.edu>
2018-09-10 13:04:38 -0700

    Initial Commit

---
+++ Streaks/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Streaks
+//
+//  Created by Chris Aguilera on 9/10/18.
+//  Copyright © 2018 Chris Aguilera. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
